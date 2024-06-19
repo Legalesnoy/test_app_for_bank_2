b65ae2a9339687da6a332d29c4d2331f75b5bd62
Oleg Urpin <LegaLesnoy@mail.ru>
2024-06-20 02:27:25 +0500

    widget.py ,main.py fixed))

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from src.masks import get_mask_account, get_mask_card_number
-
-if __name__ == "__main__":
-    print(get_mask_card_number(1234567890123456))
-    print(get_mask_account(1111111111145673))
+# from src.widget import get_data, mask_account_card
+# from src.masks import get_mask_account, get_mask_card_number
+# Кажется здесь Python ругается на циклическую ссылку
+#
+# if __name__ == "__main__":
+#     print(get_mask_card_number(1234567890123456))
+#     print(get_mask_account(1111111111145673))
+#
+#     s1 = "Счет 73654108430135874305"
+#     s2 = "Maestro 7000 7922 8960 6361"
+#     s3 = "Visa Platinum 7000 7922 8960 6361"
+#     s4 = "Счёт 7000 7922 8960 6361"
+#
+#     print(mask_account_card(s1))
+#     print(mask_account_card(s2))
+#     print(mask_account_card(s3))
+#     print(mask_account_card(s4))
+#
+#     print(get_data("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,30 @@
 from masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(card_number: str) -> str:
     """Функция общей маскировки карты и счета"""
-    card_type=["visa platinum", "maestro", ]
-    account_type = ["счёт", "счет", ]
+    card_type = [
+        "visa platinum",
+        "maestro",
+    ]
+    account_type = [
+        "счёт",
+        "счет",
+    ]
 
     number_str = ""
-    number = 0
     card_account = ""
     card_num_lst = card_number.split()
     mask_account_card = ""
 
     for word in card_num_lst:
         if word.isdigit():
-            number_str+=word
+            number_str += word
         else:
             card_account += word + " "
 
-    card_account=card_account.strip()
-    number =  int(number_str)
+    card_account = card_account.strip()
+    number = int(number_str)
 
     if card_account.lower() in card_type:
         mask_account_card = get_mask_card_number(number)
@@ -30,26 +37,27 @@ def mask_account_card(card_number: str) -> str:
 def get_data(str_with_data: str) -> str:
     """Функция преобразования даты"""
 
-    data_dct = {'year'  : str_with_data[0:4],
-                'month' : str_with_data[5:7],
-                'day'   : str_with_data[8:10], }
+    data_dct = {
+        "year": str_with_data[0:4],
+        "month": str_with_data[5:7],
+        "day": str_with_data[8:10],
+    }
 
-    data = '.'.join(list(data_dct.values())[::-1])
-
+    data = ".".join(list(data_dct.values())[::-1])
 
     return data
 
 
-if __name__ == "__main__":
-     s1 = "Счет 73654108430135874305"
-     s2 = "Maestro 7000 7922 8960 6361"
-     s3 = "Visa Platinum 7000 7922 8960 6361"
-     s4 = "Счёт 7000 7922 8960 6361"
-
-
-    print(mask_account_card(s1))
-    print(mask_account_card(s2))
-    print(mask_account_card(s3))
-    print(mask_account_card(s4))
-
-    print(get_data('2018-07-11T02:26:18.671407'))
+# if __name__ == "__main__":
+#      s1 = "Счет 73654108430135874305"
+#      s2 = "Maestro 7000 7922 8960 6361"
+#      s3 = "Visa Platinum 7000 7922 8960 6361"
+#      s4 = "Счёт 7000 7922 8960 6361"
+#
+#
+#     print(mask_account_card(s1))
+#     print(mask_account_card(s2))
+#     print(mask_account_card(s3))
+#     print(mask_account_card(s4))
+#
+#     print(get_data('2018-07-11T02:26:18.671407'))
