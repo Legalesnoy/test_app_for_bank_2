87f086b351e1afa0dd0385c2d8c875f3327aacec
Oleg Urpin <LegaLesnoy@mail.ru>
2024-06-20 01:36:43 +0500

    def mask_account_card+ , masks.py updated

--- src/masks.py
+++ src/masks.py
@@ -18,25 +18,3 @@ def get_mask_account(account_number: int) -> str:
 
     return mask_account
 
-
-def get_mask_card_number2(card_number: int) -> str:
-    """Функция маскирует номер карты и возвращается в формате XXXX XX** **** XXXX"""
-    mask_card_number = list('XXXX XX** **** XXXX')
-    mask_card_number = list(str(card_number)[0:4])
-    # [mask_card_number.insert((i+1)*4,' ') for i in range(3)]
-    s1=list(str(card_number))
-    [s1.insert(((i+1)*4)+i,' ') for i in range(4)]
-    [s1.insert(((i + 1) * 4) + i, ' ') for i in range(4)]
-
-    print(''.join(s1))
-    # print(f'{str(card_number)[0:4]} {str(card_number)[5:7]}** **** {str(card_number)[12:]}')
-
-    # for i, symb in enumerate(list(str(card_number))):
-    #     if i % 4 == 0:
-    #         mask_card_number += " "
-    #     if 5 < i < 12:
-    #         symb = "*"
-    #     mask_card_number += symb
-    #
-    # return mask_card_number.lstrip(" ")
-print (get_mask_card_number2(123456789012345678))
--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,30 @@
-def mask_account_card():
+from masks import get_mask_account, get_mask_card_number
+def mask_account_card(card_number: str) -> str:
     """Функция общей маскировки карты и счета"""
-    ...
+    card_type=["visa platinum", "maestro", ]
+    account_type = ["счёт", "счет", ]
+
+    number_str = ""
+    number = 0
+    card_account = ""
+    card_num_lst = card_number.split()
+    mask_account_card = ""
+
+    for word in card_num_lst:
+        if word.isdigit():
+            number_str+=word
+        else:
+            card_account += word + " "
+
+    card_account=card_account.strip()
+    number =  int(number_str)
+    
+    if card_account.lower() in card_type:
+        mask_account_card = get_mask_card_number(number)
+    elif card_account.lower() in account_type:
+        mask_account_card = get_mask_account(number)
+
+    return mask_account_card
 
 
 def get_data():
@@ -8,3 +32,13 @@ def get_data():
     Функция преобразования даты
     """
     ...
+
+s1 = "Счет 73654108430135874305"
+s2 = "Maestro 7000 7922 8960 6361"
+s3 = "Visa Platinum 7000 7922 8960 6361"
+s4 = "Счёт 7000 7922 8960 6361"
+
+print(mask_account_card(s1))
+print(mask_account_card(s2))
+print(mask_account_card(s3))
+print(mask_account_card(s4))
