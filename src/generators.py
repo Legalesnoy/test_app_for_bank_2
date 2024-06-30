5b2e28c0494de7302698a06aa2cf12365e45b2ec
Oleg Urpin <LegaLesnoy@mail.ru>
2024-07-01 01:12:13 +0500

    generators.py & test_generators add

---
+++ src/generators.py
@@ -0,0 +1,155 @@
+'''функции для работы с массивами транзакций'''
+from typing import List, Iterable
+
+from src.processing import sort_by_date
+
+def sign(x: int) -> int:
+    ''' знак числа'''
+
+    if x == 0: return 0
+    return int(x / abs(x))
+
+
+def filter_by_currency(transactions: List, currency: str) ->  Iterable:
+    ''' функция принимает список словарей с банковскими операциями
+    (или объект-генератор, который выдает по одной банковской операции)
+    и возвращает итератор, который выдает по очереди операции,
+    в которых указана заданная валюта.'''
+    k1 = 'operationAmount'
+    k2 = 'currency'
+    k3 = 'code'
+    return filter(lambda v : v[k1][k2][k3] == currency, sort_by_date(transactions))
+
+
+def transaction_descriptions(transactions: List) -> Iterable:
+
+    '''функция генератор, который принимает список словарей
+    и возвращает описание каждой операции по очереди.'''
+
+    return map(lambda v: v['description'], sort_by_date(transactions))
+
+
+def card_number_generator(n_min: int, n_max: int) -> Iterable[str]:
+    '''функция генератор номеров банковских карт,
+       который должен генерировать номера карт в формате
+       XXXX XXXX XXXX XXXX, где X — цифра
+       диапазоны n_min, n_max
+       '''
+    n_max += sign(n_max - n_min)
+    for i in range(n_min, n_max, sign(n_max - n_min)):
+        card_number = '0' * (16 - len(str(i))) + str(i)
+        card_number = f'{card_number[0:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}'
+        yield card_number
+
+
+# # проверочка:
+# if __name__ == '__main__':
+#
+#     '''
+#     Пример списка словарей, для которых должны работать функции модуля
+#     generators:
+#     '''
+#
+#     transactions = (
+#         [
+#            {
+#                "id": 939719570,
+#                "state": "EXECUTED",
+#                "date": "2018-06-30T02:08:58.425572",
+#                "operationAmount": {
+#                    "amount": "9824.07",
+#                    "currency": {
+#                        "name": "USD",
+#                        "code": "USD"
+#                    }
+#                },
+#                "description": "Перевод организации",
+#                "from": "Счет 75106830613657916952",
+#                "to": "Счет 11776614605963066702"
+#            },
+#            {
+#                "id": 142264268,
+#                "state": "EXECUTED",
+#                "date": "2019-04-04T23:20:05.206878",
+#                "operationAmount": {
+#                    "amount": "79114.93",
+#                    "currency": {
+#                        "name": "USD",
+#                        "code": "USD"
+#                    }
+#                },
+#                "description": "Перевод со счета на счет",
+#                "from": "Счет 19708645243227258542",
+#                "to": "Счет 75651667383060284188"
+#            },
+#            {
+#                "id": 873106923,
+#                "state": "EXECUTED",
+#                "date": "2019-03-23T01:09:46.296404",
+#                "operationAmount": {
+#                    "amount": "43318.34",
+#                    "currency": {
+#                        "name": "руб.",
+#                        "code": "RUB"
+#                    }
+#                },
+#                "description": "Перевод со счета на счет",
+#                "from": "Счет 44812258784861134719",
+#                "to": "Счет 74489636417521191160"
+#            },
+#            {
+#                "id": 895315941,
+#                "state": "EXECUTED",
+#                "date": "2018-08-19T04:27:37.904916",
+#                "operationAmount": {
+#                    "amount": "56883.54",
+#                    "currency": {
+#                        "name": "USD",
+#                        "code": "USD"
+#                    }
+#                },
+#                "description": "Перевод с карты на карту",
+#                "from": "Visa Classic 6831982476737658",
+#                "to": "Visa Platinum 8990922113665229"
+#            },
+#            {
+#                "id": 594226727,
+#                "state": "CANCELED",
+#                "date": "2018-09-12T21:27:25.241689",
+#                "operationAmount": {
+#                    "amount": "67314.70",
+#                    "currency": {
+#                        "name": "руб.",
+#                        "code": "RUB"
+#                    }
+#                },
+#                "description": "Перевод организации",
+#                "from": "Visa Platinum 1246377376343588",
+#                "to": "Счет 14211924144426031657"
+#            }
+#          ]
+#    )
+#
+#     usd_transactions = filter_by_currency(transactions, "USD")
+#     for _ in transactions:
+#         print(next(usd_transactions,{}).get('id'))
+#     #939719570
+#     #142264268
+#
+#     descriptions = transaction_descriptions(transactions)
+#     for _ in transactions:
+#         print(next(descriptions))
+#     # Перевод организации
+#     # Перевод со счета на счет
+#     # Перевод со счета на счет
+#     # Перевод с карты на карту
+#     # Перевод организации
+#
+#
+#     for card_number in card_number_generator(0, 5):
+#         print(card_number)
+#         # 0000 0000 0000 0001
+#         # 0000 0000 0000 0002
+#         # 0000 0000 0000 0003
+#         # 0000 0000 0000 0004
+#         # 0000 0000 0000 0005
---
+++ tests/test_generators.py
@@ -0,0 +1,25 @@
+import pytest
+
+from src.generators import sign, filter_by_currency, card_number_generator
+
+
+@pytest.mark.parametrize("n, expected", [(-10,-1), (0,0), (2323,1)])
+def test_sign(n, expected):
+    assert sign(n) == expected
+
+
+def test_filter_by_currency(transactions):
+    usd_transactions = filter_by_currency(transactions, "USD")
+    result = list(usd_transactions)
+    expected_result = [transactions[1], transactions[3], transactions[0]]
+
+    assert result == expected_result
+
+
+def test_card_number_generator():
+    expected_result = ['0000 0000 0000 0001',
+                       '0000 0000 0000 0002',
+                       '0000 0000 0000 0003',
+                       '0000 0000 0000 0004',
+                       '0000 0000 0000 0005']
+    assert list(card_number_generator(1, 5)) == expected_result
